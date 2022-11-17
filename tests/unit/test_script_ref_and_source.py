e19150602b8274077b592f8ff716ab68bd11aef6
Meghan <msheintz@gmail.com>
2022-11-17 17:44:51 -0500

    variables unit test

--- tests/unit/test_script_ref_and_source.py
+++ tests/unit/test_script_ref_and_source.py
@@ -96,13 +96,39 @@ TESTS_INTEGRATION = (
     ),
     (
         """
-    WITH aa as
-    (SELECT *
-    FROM  {{ ref("ref1") }} bb)
-    SELECT *
-    from aa
+        {% set dao_proposals_models = [
+        'uniswap_v3_ethereum_proposals'
+        ] %}
+        
+        SELECT *
+        FROM (
+            {% for dao_model in dao_proposals_models %}
+            SELECT
+                blockchain,
+                project,
+                version,
+                created_at,
+                tx_hash,
+                dao_name,
+                dao_address,
+                proposer,
+                proposal_id,
+                votes_for,
+                votes_against,
+                votes_abstain,
+                votes_total,
+                number_of_voters,
+                participation,
+                status,
+                description
+            FROM {{ ref(dao_model) }}
+            {% if not loop.last %}
+            UNION ALL
+            {% endif %}
+            {% endfor %}
+        )
     """,
-        1,
+        0,
         True,
     ),
 )
